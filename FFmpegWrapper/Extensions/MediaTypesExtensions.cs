0d768fbb5d7c8f0404e1571b6b5a1a81a3ab07df
Bank <robertobl022@gmail.com>
2022-02-08 23:46:25 +0100

    Use data format to get codec output directly

--- FFmpegWrapper/Extensions/MediaTypesExtensions.cs
+++ FFmpegWrapper/Extensions/MediaTypesExtensions.cs
@@ -8,6 +8,7 @@ namespace FFmpegWrapper.Extensions
         internal static string GetOutPutArgs(this MediaTypes mediaTypes) => mediaTypes switch
         {
             MediaTypes.MP4 => $"-f {mediaTypes} -movflags empty_moov pipe:",
+            MediaTypes.OPUS => $"-map 0 -f data -c:a libopus -b:a 96K pipe:",
             _ => $"-f {mediaTypes} pipe:"
         };
 
