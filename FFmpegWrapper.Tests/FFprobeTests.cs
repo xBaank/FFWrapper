c99d5799ae02f04edb9da22a52256f0e6b498433
Bank <robertobl022@gmail.com>
2022-03-03 09:12:30 +0100

    Add  ffprobe tests that doesnt use input pipe

--- FFmpegWrapper.Tests/FFprobeTests.cs
+++ FFmpegWrapper.Tests/FFprobeTests.cs
@@ -1,94 +1,94 @@
-﻿//using FFmpegWrapper.Formats;
-//using FFmpegWrapper.Models;
-//using FFmpegWrapper.Tests.Data;
+﻿using FFmpegWrapper.Formats;
+using FFmpegWrapper.Models;
+using FFmpegWrapper.Tests.Data;
 
-//using System.IO;
-//using System.Net.Http;
+using System.IO;
+using System.Net.Http;
 
-//using Xunit;
+using Xunit;
 
-//namespace FFmpegWrapper.Tests
-//{
-//    public class FFprobeTests
-//    {
-//        private FFprobeClient ffProbeClient = new FFprobeClient();
-//        HttpClient httpClient = new HttpClient();
+namespace FFmpegWrapper.Tests
+{
+    public class FFprobeTests
+    {
+        private FFprobeClient ffProbeClient = new FFprobeClient();
+        HttpClient httpClient = new HttpClient();
 
 
-//        [Theory]
-//        [InlineData(VideoFilesUri.WMV)]
-//        [InlineData(AudioFilesUri.OGG)]
-//        public async void FileShouldGetFormatFromUri(string uri)
-//        {
-//            var metadata = await ffProbeClient.GetMetadataAsync(uri);
+        [Theory]
+        [InlineData(VideoFilesUri.WMV)]
+        [InlineData(AudioFilesUri.OGG)]
+        public async void FileShouldGetFormatFromUri(string uri)
+        {
+            var metadata = await ffProbeClient.GetMetadataAsync(uri);
 
-//            Assert.NotNull(metadata);
-//        }
+            Assert.NotNull(metadata);
+        }
 
-//        [Theory]
-//        [InlineData(VideoFilesUri.WMV)]
-//        [InlineData(AudioFilesUri.OGG)]
-//        public async void FileShouldGetFormatFromUriToStream(string uri)
-//        {
-//            MemoryStream ms = new MemoryStream();
-//            var metadata = await ffProbeClient.GetMetadataAsync(uri, ms);
+        [Theory]
+        [InlineData(VideoFilesUri.WMV)]
+        [InlineData(AudioFilesUri.OGG)]
+        public async void FileShouldGetFormatFromUriToStream(string uri)
+        {
+            MemoryStream ms = new MemoryStream();
+            var metadata = await ffProbeClient.GetMetadataAsync(uri, ms);
 
-//            Assert.True(ms.Length > 0 && metadata is not null);
-//        }
+            Assert.True(ms.Length > 0 && metadata is not null);
+        }
 
-//        [Theory]
-//        [InlineData(VideoFilesUri.WMV)]
-//        [InlineData(AudioFilesUri.OGG)]
-//        public async void FileShouldGetFormatFromStream(string uri)
-//        {
-//            var stream = await httpClient.GetStreamAsync(uri);
-//            var metadata = await ffProbeClient.GetMetadataAsync(stream);
+        [Theory]
+        [InlineData(VideoFilesUri.WMV)]
+        [InlineData(AudioFilesUri.OGG)]
+        public async void FileShouldGetFormatFromStream(string uri)
+        {
+            var stream = await httpClient.GetStreamAsync(uri);
+            var metadata = await ffProbeClient.GetMetadataAsync(stream);
 
-//            Assert.NotNull(metadata);
-//        }
+            Assert.NotNull(metadata);
+        }
 
-//        [Theory]
-//        [InlineData(VideoFilesUri.WMV, StreamType.v)]
-//        [InlineData(AudioFilesUri.OGG, StreamType.a)]
-//        public async void FileShouldGetPacketsAndFramesFromUri(string uri, StreamType streamType)
-//        {
-//            var packets = await ffProbeClient.GetPacketsAsync(uri, streamType);
-//            var frames = await ffProbeClient.GetFramesAsync(uri, streamType);
+        [Theory]
+        [InlineData(VideoFilesUri.WMV, StreamType.v)]
+        [InlineData(AudioFilesUri.OGG, StreamType.a)]
+        public async void FileShouldGetPacketsAndFramesFromUri(string uri, StreamType streamType)
+        {
+            var packets = await ffProbeClient.GetPacketsAsync(uri, streamType);
+            var frames = await ffProbeClient.GetFramesAsync(uri, streamType);
 
-//            Assert.True(packets?.Count > 0);
-//            Assert.True(frames?.Count > 0);
-//        }
+            Assert.True(packets?.Count > 0);
+            Assert.True(frames?.Count > 0);
+        }
 
-//        [Theory]
-//        [InlineData(VideoFilesUri.MOV, StreamType.v)]
-//        [InlineData(AudioFilesUri.OGG, StreamType.a)]
-//        public async void FileShouldGetPacketsAndFramesFromUriToStream(string uri, StreamType streamType)
-//        {
-//            var ms = new MemoryStream();
+        [Theory]
+        [InlineData(VideoFilesUri.MOV, StreamType.v)]
+        [InlineData(AudioFilesUri.OGG, StreamType.a)]
+        public async void FileShouldGetPacketsAndFramesFromUriToStream(string uri, StreamType streamType)
+        {
+            var ms = new MemoryStream();
 
-//            var packets = await ffProbeClient.GetPacketsAsync(uri, streamType, ms);
-//            Assert.True(ms.Length > 0 && packets?.Count > 0);
+            var packets = await ffProbeClient.GetPacketsAsync(uri, streamType, ms);
+            Assert.True(ms.Length > 0 && packets?.Count > 0);
 
-//            ms = new MemoryStream();
+            ms = new MemoryStream();
 
-//            var frames = await ffProbeClient.GetFramesAsync(uri, streamType, ms);
-//            Assert.True(ms.Length > 0 && frames?.Count > 0);
-//        }
+            var frames = await ffProbeClient.GetFramesAsync(uri, streamType, ms);
+            Assert.True(ms.Length > 0 && frames?.Count > 0);
+        }
 
-//        [Theory]
-//        [InlineData(VideoFilesUri.WMV, StreamType.v)]
-//        [InlineData(VideoFilesUri.WEBM, StreamType.v)]
-//        public async void FileShouldGetPacketsAndFramesFromStream(string uri, StreamType streamType)
-//        {
-//            var bytes = await httpClient.GetStreamAsync(uri);
+        [Theory]
+        [InlineData(VideoFilesUri.WMV, StreamType.v)]
+        [InlineData(VideoFilesUri.WEBM, StreamType.v)]
+        public async void FileShouldGetPacketsAndFramesFromStream(string uri, StreamType streamType)
+        {
+            var bytes = await httpClient.GetStreamAsync(uri);
 
-//            var packets = await ffProbeClient.GetPacketsAsync(bytes, streamType);
+            var packets = await ffProbeClient.GetPacketsAsync(bytes, streamType);
 
-//            bytes = await httpClient.GetStreamAsync(uri);
-//            var frames = await ffProbeClient.GetFramesAsync(bytes, streamType);
+            bytes = await httpClient.GetStreamAsync(uri);
+            var frames = await ffProbeClient.GetFramesAsync(bytes, streamType);
 
-//            Assert.True(packets?.Count > 0);
-//            Assert.True(frames?.Count > 0);
-//        }
-//    }
-//}
+            Assert.True(packets?.Count > 0);
+            Assert.True(frames?.Count > 0);
+        }
+    }
+}
